279563d9c1f44f562ceb1716e4f2e9cd17fd21fe
Aysan Isayo <aisayo@icloud.com>
2022-03-16 22:51:47 -0700

    added components and tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+
 function App() {
-  return <main></main>;
+  return (
+    <main className="text-rose-900 bg-red-50 font-serif flex flex-col h-screen">
+      <NavBar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  );
 }
 
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,41 @@
 function About() {
   return (
-    <div>About</div>
-  )
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-rose-400">
+            Hey! I'm Aysan
+            <br className="hidden lg:inline-block" />I enjoy building dynamic
+            full stack web applications!
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Need to replace with a self summary
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-red-300 border-0 py-2 px-6 focus:outline-none hover:bg-rose-900 rounded text-lg"
+            >
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-white bg-red-300 border-0 py-2 px-6 focus:outline-none hover:bg-rose-900 hover:text-white rounded text-lg"
+            >
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          {/* <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./coding.svg"
+          /> */}
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
